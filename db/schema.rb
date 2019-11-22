7b44ea424ef5327767f2d41ce231332ed6d92514
Luckey <lachlanharpley@gmail.com>
2019-11-22 21:31:51 +0100

    d1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_191541) do
+ActiveRecord::Schema.define(version: 2019_11_22_202732) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
