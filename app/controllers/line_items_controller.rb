618db46587e2f9b5e5428cb60f1c73da630241f9
Luckey <lachlanharpley@gmail.com>
2019-12-14 14:01:14 +0100

    try to add new method

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,9 +59,20 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    binding.pry
     @line_item.destroy
     respond_to do |format|
       format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  
+  def destroy_from_cart
+    @line_item.destroy
+    binding.pry
+    
+    respond_to do |format|
+      format.html { redirect_to cart_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :line_items
+  delete '/line_items/:id', to: 'line_items#destroy_from_cart', as: 'destroy_from_cart'
+
   resources :carts
   root 'store#index', as: 'store_index'
+  
+
   
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
